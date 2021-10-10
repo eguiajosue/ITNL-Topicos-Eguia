04b425b19a272823bc7276292ce578c73027e63b
Josue Eguia <eguiajosue9@gmail.com>
2021-10-09 21:28:10 -0500

    Colors added

--- src/Ejercicio1/Quiz.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio1;
-
-import java.awt.event.*;
-import java.awt.*;
-
-public class Quiz {
-	
-	
-}
--- src/Ejercicio1/QuizApp.java
+++ src/Ejercicio1/QuizApp.java
@@ -18,7 +18,7 @@ import javax.swing.JRadioButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class QuizUI {
+public class QuizApp {
 	
 	
 	String[] preguntas = 	{
@@ -49,10 +49,11 @@ public class QuizUI {
 	
 	public void siguientePregunta() {
 		if (index >= totalPreguntas) {
-			btnSiguiente.setEnabled(false);
+			JOptionPane.showMessageDialog(null, "No hay mas preguntas");
 		}
 		else {
 			lblPregunta.setText(preguntas[index]);
+			
 			radRespuestaA.setText(respuestas[index][0]);
 			radRespuestaB.setText(respuestas[index][1]);
 			radRespuestaC.setText(respuestas[index][2]);
@@ -81,7 +82,7 @@ public class QuizUI {
 			radRespuestaE.setForeground(new Color(255, 0, 0)); 
 		*/
 		
-		Timer espera = new Timer(2000, new ActionListener() {
+		Timer espera = new Timer(1000, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				/* REESTABLECER COLORES
@@ -105,8 +106,39 @@ public class QuizUI {
 	
 	
 	public void mostrarResultados() {
+		radRespuestaA.setEnabled(false);
+		radRespuestaB.setEnabled(false);
+		radRespuestaC.setEnabled(false);
+		radRespuestaD.setEnabled(false);
+		radRespuestaE.setEnabled(false);
+		btnAnterior.setEnabled(false);
+		btnSiguiente.setEnabled(false);
+		
 		resultadoFinal = (preguntasCorrectas/(double)totalPreguntas * 100);
-		JOptionPane.showMessageDialog(null, "CALIFICACIÓN FINAL: " + resultadoFinal + "\nACIERTOS: " + preguntasCorrectas + "/" + totalPreguntas);
+		String totalAciertos = preguntasCorrectas + "/" + totalPreguntas;
+		
+		lblCantidadCalificacion.setText(String.valueOf(resultadoFinal));
+		lblCantidadAciertos.setText(totalAciertos);
+		
+		if (resultadoFinal >= 70 ) {
+			lblCantidadCalificacion.setForeground(new Color(34,139,34));
+		} else if (resultadoFinal < 70 && resultadoFinal >= 50) {
+			lblCantidadCalificacion.setForeground(new Color(204, 204, 0));
+		} else {
+			lblCantidadCalificacion.setForeground(new Color(255, 0, 0));
+		}
+		
+		
+		if (resultadoFinal >= 4 ) {
+			lblCantidadAciertos.setForeground(new Color(34,139,34));
+		} else if (resultadoFinal < 4 && resultadoFinal >= 2) {
+			lblCantidadAciertos.setForeground(new Color(204, 204, 0));
+		} else {
+			lblCantidadAciertos.setForeground(new Color(255, 0, 0));
+		}
+		
+		// JOptionPane.showMessageDialog(null, "CALIFICACIÓN FINAL: " + resultadoFinal + "\nACIERTOS: " + preguntasCorrectas + "/" + totalPreguntas);
+		
 	}
 	
 	private JFrame frmQuizApp;
@@ -119,12 +151,16 @@ public class QuizUI {
 	private JButton btnVerResultados;
 	private JRadioButton radRespuestaE;
 	private JButton btnAnterior;
+	private JLabel lblCalificacion;
+	private JLabel lblAciertos;
+	private JLabel lblCantidadCalificacion;
+	private JLabel lblCantidadAciertos;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					QuizUI window = new QuizUI();
+					QuizApp window = new QuizApp();
 					window.frmQuizApp.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -133,7 +169,7 @@ public class QuizUI {
 		});
 	}
 
-	public QuizUI() {
+	public QuizApp() {
 		initialize();
 	}
 
@@ -148,31 +184,31 @@ public class QuizUI {
 		JLabel lblTitulo = new JLabel("Quiz App");
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitulo.setFont(new Font("Rubik", Font.BOLD, 45));
-		lblTitulo.setBounds(10, 11, 470, 55);
+		lblTitulo.setBounds(10, 11, 250, 55);
 		frmQuizApp.getContentPane().add(lblTitulo);
 		
 		lblPregunta = new JLabel("Pregunta #");
-		lblPregunta.setBounds(10, 77, 614, 14);
+		lblPregunta.setBounds(10, 77, 534, 14);
 		frmQuizApp.getContentPane().add(lblPregunta);
 		
 		radRespuestaA = new JRadioButton("Respuesta A");
-		radRespuestaA.setBounds(20, 98, 109, 23);
+		radRespuestaA.setBounds(20, 98, 524, 23);
 		frmQuizApp.getContentPane().add(radRespuestaA);
 		
 		radRespuestaB = new JRadioButton("Respuesta B");
-		radRespuestaB.setBounds(20, 124, 109, 23);
+		radRespuestaB.setBounds(20, 124, 524, 23);
 		frmQuizApp.getContentPane().add(radRespuestaB);
 		
 		radRespuestaC = new JRadioButton("Respuesta C");
-		radRespuestaC.setBounds(20, 150, 109, 23);
+		radRespuestaC.setBounds(20, 150, 524, 23);
 		frmQuizApp.getContentPane().add(radRespuestaC);
 		
 		radRespuestaD = new JRadioButton("Respuesta D");
-		radRespuestaD.setBounds(20, 176, 109, 23);
+		radRespuestaD.setBounds(20, 176, 524, 23);
 		frmQuizApp.getContentPane().add(radRespuestaD);
 		
 		radRespuestaE = new JRadioButton("Respuesta E");
-		radRespuestaE.setBounds(20, 202, 109, 23);
+		radRespuestaE.setBounds(20, 202, 524, 23);
 		frmQuizApp.getContentPane().add(radRespuestaE);
 		
 		btnSiguiente = new JButton("Siguiente");
@@ -223,13 +259,15 @@ public class QuizUI {
 		btnAnterior.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				index--;
+				--index;
 				lblPregunta.setText(preguntas[index]);
 				radRespuestaA.setText(respuestas[index][0]);
 				radRespuestaB.setText(respuestas[index][1]);
 				radRespuestaC.setText(respuestas[index][2]);
 				radRespuestaD.setText(respuestas[index][3]);
 				radRespuestaE.setText(respuestas[index][4]);
+				
+				respuesta = ' ';
 			}
 		});
 		btnAnterior.setBounds(314, 232, 110, 31);
@@ -254,6 +292,30 @@ public class QuizUI {
 		});
 		btnVerResultados.setBounds(10, 274, 534, 31);
 		frmQuizApp.getContentPane().add(btnVerResultados);
+		
+		lblCalificacion = new JLabel("Calificaci\u00F3n: ");
+		lblCalificacion.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCalificacion.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCalificacion.setBounds(327, 11, 97, 14);
+		frmQuizApp.getContentPane().add(lblCalificacion);
+		
+		lblAciertos = new JLabel("Aciertos: ");
+		lblAciertos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAciertos.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAciertos.setBounds(327, 36, 97, 14);
+		frmQuizApp.getContentPane().add(lblAciertos);
+		
+		lblCantidadCalificacion = new JLabel("0");
+		lblCantidadCalificacion.setHorizontalAlignment(SwingConstants.LEFT);
+		lblCantidadCalificacion.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCantidadCalificacion.setBounds(434, 11, 97, 14);
+		frmQuizApp.getContentPane().add(lblCantidadCalificacion);
+		
+		lblCantidadAciertos = new JLabel("0");
+		lblCantidadAciertos.setHorizontalAlignment(SwingConstants.LEFT);
+		lblCantidadAciertos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCantidadAciertos.setBounds(434, 36, 97, 14);
+		frmQuizApp.getContentPane().add(lblCantidadAciertos);
 		
 		
 		
